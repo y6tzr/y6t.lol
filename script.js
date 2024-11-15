5bd4f8e36bf31e70fbe5ab49f9e59a51eee258d3
mear <162515367+y6tzr@users.noreply.github.com>
2024-11-16 02:19:45 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.social-links a').forEach(link => {
-  link.addEventListener('mouseenter', () => {
-    link.style.transform = 'scale(1.1)';
-  });
-
-  link.addEventListener('mouseleave', () => {
-    link.style.transform = 'scale(1)';
-  });
-});
