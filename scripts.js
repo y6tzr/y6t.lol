2f1cc07a5f34c14e120ac18d10c626e1b2e441cd
mear <162515367+y6tzr@users.noreply.github.com>
2024-12-16 02:12:14 +0800

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-
-window.addEventListener('load', function() {
-    var audio = document.getElementById('myAudio');
-    
-    
-    audio.play().catch(function(error) {
-        console.log('Autoplay was blocked by the browser. Attempting to play manually...');
-    });
-});
